d8706b8a77a821e00c2033e3a134f58fd5d13f3d
hassanaitoundjar <hassanprof96@gmail.com>
2023-07-05 23:54:03 +0100

    hide the product ID and display the product name

--- products.php
+++ products.php
@@ -162,7 +162,7 @@ include('./admin/include/navbar.php')
                                     <?php while ($row = mysqli_fetch_assoc($result)): ?>
                                     <tr>
                                         <td><a
-                                                href="<?php echo "https://iptvsmartersproo.com//checkout.php?product=" . $row['id']; ?>">
+                                                href="<?php echo "https://iptvsmartersproo.com/checkout.php?product=" . $row['id']; ?>">
                                                 <?php echo $row["name"]; ?>
                                             </a>
                                         </td>
